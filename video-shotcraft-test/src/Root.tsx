e3e9781fe53c298582b130136b3f90f2f6a16ad6
Seantee9163 <sean_tee@live.com>
2026-09-06 01:23:15 +0800

    feat: register 15 30 60 second Shotcraft compositions

--- video-shotcraft-test/src/Root.tsx
+++ video-shotcraft-test/src/Root.tsx
@@ -1,15 +1,45 @@
 import React from 'react';
 import {Composition} from 'remotion';
 import {defaultGoldJewelryProps, GoldJewelryVideo} from './GoldJewelryVideo';
+import {defaultShotcraftStudioProps, ShotcraftStudioVideo} from './ShotcraftStudioVideo';
 
 export const VideoRoot: React.FC = () => (
-  <Composition
-    id="GoldJewelry15s"
-    component={GoldJewelryVideo}
-    defaultProps={defaultGoldJewelryProps}
-    durationInFrames={450}
-    fps={30}
-    width={1080}
-    height={1920}
-  />
+  <>
+    <Composition
+      id="GoldJewelry15s"
+      component={GoldJewelryVideo}
+      defaultProps={defaultGoldJewelryProps}
+      durationInFrames={450}
+      fps={30}
+      width={1080}
+      height={1920}
+    />
+    <Composition
+      id="Shotcraft15s"
+      component={ShotcraftStudioVideo}
+      defaultProps={defaultShotcraftStudioProps}
+      durationInFrames={450}
+      fps={30}
+      width={1080}
+      height={1920}
+    />
+    <Composition
+      id="Shotcraft30s"
+      component={ShotcraftStudioVideo}
+      defaultProps={defaultShotcraftStudioProps}
+      durationInFrames={900}
+      fps={30}
+      width={1080}
+      height={1920}
+    />
+    <Composition
+      id="Shotcraft60s"
+      component={ShotcraftStudioVideo}
+      defaultProps={defaultShotcraftStudioProps}
+      durationInFrames={1800}
+      fps={30}
+      width={1080}
+      height={1920}
+    />
+  </>
 );
